d121e65d13cfc9d224e72652ded38c252cb8c9f4
Mayank SInha <mayanksinha2018@gmail.com>
2021-09-28 20:59:15 +0530

    Add psycopg2-binary-2.9.1 in requirments.txt

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ Bootstrap(app)
 csrf = CSRFProtect(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
